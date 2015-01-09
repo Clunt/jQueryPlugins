6418c13f0f832ad48addd3fd9fc88f97ce7b406c
Clunt <clunt@foxmail.com>
2015-01-09 04:15:47 -0800

    Add Select Cascade V2

---
+++ select-cascade-v2/cascade.js
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) 2014 Clunt $.fn.cascade
+ * Licensed under the MIT License
+ */
+(function(factory) {
+  factory(jQuery);
+})(function($) {
+  if (typeof $.fn.cascade !== 'undefined') {
+    return;
+  }
+  $.fn.cascade = function(option) {
+    function Cascade(node, data) {
+      this.$node = $(node);
+      this.node  = node;
+      this.data  = data || {};
+      this.map   = [];
+      this.init();
+    }
+    Cascade.prototype.init = function() {
+      this.anaylseStructure();
+      this.bind();
+    };
+    Cascade.prototype.bind = function() {
+      var self = this;
+      this.$node.on('change', function() {
+        var value = $(this).val(),
+            data  = self.data;
+        if (value !== null) {
+          if (value === '') {
+            self.$node.nextAll().remove();
+          } else if (typeof data[value] === 'string') {
+            self.ajaxCascade(value);
+          } else if (typeof data[value] === 'object') {
+            self.dealCascade(value);
+          }
+        }
+      });
+    };
+    Cascade.prototype.anaylseStructure = function() {
+      var node    = this.node,
+          options = node.options,
+          path    = '',
+          option,
+          value;
+      for (var i = 0; i < options.length; i += 1) {
+        option = options[i];
+        value = options[i].value;
+        if (value !=='') {
+          if (this.data[value] === undefined) {
+            this.data[value] = path;
+          }
+        }
+      }
+    };
+    Cascade.prototype.dealCascade = function(value) {
+      var self = this;
+      var index = [];// TODO
+      var handle = {
+        input: function (data) {
+          var html = '<input name="" value="">';
+          return html;
+        },
+        textarea: function (data) {
+          var html = '<textarea name=""></textarea>';
+          return html;
+        },
+        select: function (data) {
+          var options = data.option;
+          var option;
+          var html = '<select name="">';
+          for (var i = 0; i < options.length; i += 1) {
+            option = options[i];
+            html += '<option value="' + option.value + '">' + option.text + '</option>';
+          }
+          html += '</select>'
+          return html;
+        }
+      };
+      var $html = $(function() {
+        var cascades = self.data[value];
+        var cascade;
+        var html = '';
+        for (var i = 0; i < cascades.length; i++) {
+          cascade = cascades[i];
+          html += handle[cascade.type](cascade);
+
+          // TODO
+          if (cascade.type == 'select') {
+            index.push({
+              value: value,
+              ind: i
+            });
+          }
+        }
+        return html;
+      }());
+      this.$node.nextAll().remove().end().after($html);
+      for (var i = 0; i < index.length; i++) {
+        var ind = index[i]['ind'];
+        var target = $html[ind];
+        if (this.map[index[i]['value']] === undefined) {
+          this.map[index[i]['value']] = {};
+        }
+        new Cascade(target, this.map[index[i]['value']]);
+      }
+    };
+
+
+
+    Cascade.prototype.ajaxCascade = function(value) {
+      console.log('ajaxCascade')
+      var self = this;
+      !(function() {
+        if (value === 'input') {
+          self.data[value] = [{
+            "type": "input"
+          }];
+        } else if (value === 'select') {
+          self.data[value] = [{
+            "type": "select",
+            "option": [{
+              "text": "美国",
+              "value": "select"
+            }, {
+              "text": "美国",
+              "value": "input"
+            }, {
+              "text": "中国",
+              "value": "textarea"
+            }]
+          }];
+        } else if (value === 'textarea') {
+          self.data[value] = [{
+            "type": "textarea"
+          }];
+        } else if (value === 'all') {
+          self.data[value] = [{
+            "type": "input"
+          }, {
+            "type": "textarea"
+          }, {
+            "type": "select",
+            "option": [{
+              "text": "美国",
+              "value": "amarica"
+            }, {
+              "text": "美国",
+              "value": "amarica"
+            }, {
+              "text": "中国",
+              "value": "china"
+            }]
+          }];
+        } else {
+          self.data[value] = [{
+            "type": "input"
+          }, {
+            "type": "textarea"
+          }];
+        }
+      }())
+      this.dealCascade(value);
+    };
+    return this.each(function(index, target) {
+      var $target = $(target);
+      if ($target.attr('data-cascade') !== undefined) {
+        return;
+      }
+      $target.attr('data-cascade', +new Date);
+      test = new Cascade(target);
+    });
+  };
+});
